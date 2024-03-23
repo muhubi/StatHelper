e57dbb1f6934cb85d0ebb1aa628681bcafabfa12
Andrew Mahabee <andrew.mahabee@proton.me>
2024-03-22 23:40:32 -0400

    added statistical analysis of data using numpy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 class stathelper_file_import:
     #opens the file
@@ -13,8 +13,28 @@ class stathelper_file_import:
     #sorts the list
     data_input_list.sort()
     #prints the sorted list
-    print(data_input_list)
+    # print(data_input_list)
+    
+    #converts the sorted list into floats
+    data_input_list_float = [float(i) for i in data_input_list]
+    #prints the sorted float list
+    # print(data_input_list_float)
+    
+    #uses NumPy to analyze the list
+    min_input_list = np.min(data_input_list_float)
+    max_input_list = np.max(data_input_list_float)
+    median_input_list = np.median(data_input_list_float)
+    mean_input_list = np.mean(data_input_list_float)
+    stdev_input_list = np.std(data_input_list_float)
 
-
-class stathelper_input_list:
+    
+    #prints the sorted float list
+    print("Here is the list in order: " + str(data_input_list_float))
+    print()
+    
+    #prints the analyses
+    print("min, max: " + str(min_input_list) + ", " + str(max_input_list))
+    print("media: " + str(median_input_list))
+    print("mean: " + str(mean_input_list))
+    print("std: " + str(stdev_input_list))
     
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+tatistics.pstdev
