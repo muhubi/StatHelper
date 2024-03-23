5e5c780ba9685c1c6ad1dd6a691ae7ec81e94dcb
Andrew Mahabee <andrew.mahabee@proton.me>
2024-03-22 21:58:05 -0400

    added code

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import numpy
 
 class StatHelper:
+    test_file = open("test_input_data.txt")
+    
+    data_test_file = test_file.read()
+    
+    data_test_list = data_test_file.split("\n")
+    print(data_test_list)
+    test_file.close()
     
