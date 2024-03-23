5265978f905aeda2510ba5f8cf5d989be91ec546
Andrew Mahabee <andrew.mahabee@proton.me>
2024-03-22 22:24:31 -0400

    rename to input data

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy
 
 class StatHelper:
-    test_file = open("test_input_data.txt")
+    test_file = open("input_data.txt")
     
     data_test_file = test_file.read()
     
     data_test_list = data_test_file.split("\n")
-    print(data_test_list)
     test_file.close()
     
+    data_test_list.sort()
+    print(data_test_list)
